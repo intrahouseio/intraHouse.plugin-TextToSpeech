836a9df9c4b82956d6f77001c27219c482e74884
Artem Ivanov <fast0490f@mail.ru>
2018-12-27 10:52:34 +0300

    init

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
-const Plugin = require('./lib/plugin');
+const crypto = require('crypto');
+const fs = require('fs');
+
 const player = require('play-sound')({});
+const yandex_speech = require('yandex-speech');
+
+const Plugin = require('./lib/plugin');
+
 
 const plugin = new Plugin();
 
+
+function yandex_tts(hash, text) {
+  yandex_speech.TTS({
+    text,
+    developer_key: '069b6659-984b-4c5f-880e-aaedcfd84102',
+    file: `./cache/${hash}.wav`,
+    format: 'wav',
+    emotions: 'neutral',
+  }, () => player.play(`./cache/${hash}.wav`));
+}
+
+function say() {
+  const text = 'Привет, мир!';
+  const hash = crypto.createHash('md5').update(text).digest("hex");
+  const path = `./cache/${hash}.wav`;
+
+  fs.exists(path, exists => {
+    if (exists) {
+      player.play(path)
+    } else {
+      yandex_tts(hash, text);
+    }
+  });
+}
 
 plugin.on('device_action', (device) => {
     plugin.debug("incomming action: " + device.id + " / " + device.command);
@@ -18,5 +48,5 @@ plugin.on('toolbar_command', (command) => {
 })
 
 plugin.on('start', () => {
-  player.play('/home/sadm/git/test/public/1.wav', (err) => console.log(err));
+  say();
 });
